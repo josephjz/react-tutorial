861c820b387ade9f9a2b945f702ad74de280078a
Jennifer Joseph <josephjz@bc.edu>
2020-12-24 12:27:19 -0500

    Hello 1

---
+++ src/sayHello.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Hello() {
+
+    const sayHello = () => {
+        console.log("Hello!")
+    };
+
+    return(
+        <div>
+            <button onClick={sayHello}>Hello Button</button>
+        </div>
+    );
+}
+export default Hello;
