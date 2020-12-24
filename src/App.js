49ab9dc797c1ccbf4276e9639e0a00e385d0f2bf
Jennifer Joseph <josephjz@bc.edu>
2020-12-24 13:04:41 -0500

    Color Changes.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
-import React from "react";
-import Tweet from './tweet';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="app">
-      <Tweet name = "Jennifer" message = "Merry"/>
-      <Tweet name = "Margaret" message = "Christmas"/>
-      <Tweet name = "Joseph" message = "Eve"/>
-    </div>
-  );
+
+    const [isRed, setRed] = useState(false);
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+        setRed(!isRed);
+    };
+
+    return (
+        <div className="app">
+            <h1 className={isRed ? "red" : ""}>Color.</h1>
+            <button onClick={increment}>Increment and Change Color</button>
+            <h1>{count}</h1>
+        </div>
+    );
 }
 
 export default App;
